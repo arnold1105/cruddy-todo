e09a5eda0f6315589a0c98b1ee75858c39328e88
Brian Lagman <bjlagman@gmail.com>
2019-01-23 23:59:49 -0800

    implemented the create function

--- datastore/index.js
+++ datastore/index.js
@@ -8,9 +8,20 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    var fileName = `${this.dataDir}/${id}.txt`;
+    fs.writeFile(fileName, text, (err) => {
+      if (err) {
+        throw ('error not creating');
+      } else {
+        callback(null, { id, text });
+      }
+    });
+  });
+
+  // var id = counter.getNextUniqueId();
+  // items[id] = text;
+  // callback(null, { id, text });
 };
 
 exports.readAll = (callback) => {
